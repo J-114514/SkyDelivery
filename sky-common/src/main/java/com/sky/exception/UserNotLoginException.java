54d9fc02aef6ab0b17ef8d5667130cbc97d26eb7
J-114514 <3250949027@qq.com>
2025-04-02 22:25:29 +0800

    项目初始代码

---
+++ sky-common/src/main/java/com/sky/exception/UserNotLoginException.java
@@ -0,0 +1,13 @@
+package com.sky.exception;
+
+
+public class UserNotLoginException extends BaseException {
+
+    public UserNotLoginException() {
+    }
+
+    public UserNotLoginException(String msg) {
+        super(msg);
+    }
+
+}
